caa2453817d4980388a4a1b178a32159ff428aa6
Philipp Vincent Ebert <philipp@ebertonline.eu>
2023-04-19 10:19:21 +0200

    added some debug prints in funtion `Reverse`

--- main.go
+++ main.go
@@ -14,9 +14,11 @@ func main() {
 }
 
 func Reverse(s string) string {
-	b := []rune(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+	fmt.Printf("input: %q\n", s)
+	r := []rune(s)
+	fmt.Printf("runes: %q\n", r)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
 	}
-	return string(b)
+	return string(r)
 }
