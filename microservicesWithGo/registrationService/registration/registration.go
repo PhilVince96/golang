0b809869a7b382a9cadc03b7637a7b37ea9020d1
Philipp Vincent Ebert <philipp@ebertonline.eu>
2023-04-19 17:08:29 +0200

    created struct `Registration`

---
+++ microservicesWithGo/registrationService/registration/registration.go
@@ -0,0 +1,12 @@
+package registration
+
+type Registration struct {
+	Firstname                  string
+	Lastname                   string
+	Email                      string
+	Company                    string
+	Trainingcode               string
+	Date                       string
+	PrivacyProctectionAccepted bool
+	// no address for now
+}
